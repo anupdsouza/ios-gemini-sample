4eea81b5979bf3f17e81d288030be9fd970aae4d
Anup DSouza <anup.dsouza@gmail.com>
2023-12-15 08:31:42 +0530

    Initial files with Gemini SPM integration

---
+++ GeminiChat/GeminiChat/GeminiChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiChatApp.swift
+//  GeminiChat
+//
+//  Created by Anup D'Souza on 15/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GeminiChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
